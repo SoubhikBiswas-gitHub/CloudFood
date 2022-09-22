dc99d8676660470ea414a0252bb69c24f8a49438
SOUBHIK BISWAS <sbiswas773@gmail.com>
2022-09-22 14:12:34 +0530

    mobile-hero update

--- index.js
+++ index.js
@@ -5,12 +5,12 @@
 const navbar = document.querySelector("[data-navbar]");
 const navbarLinks = document.querySelectorAll("[data-nav-link]");
 const menuToggleBtn = document.querySelector("[data-menu-toggle-btn]");
-console.log(navbar)
-console.log(navbarLinks)
-console.log(menuToggleBtn)
+console.log(navbar);
+console.log(navbarLinks);
+console.log(menuToggleBtn);
 
 menuToggleBtn.addEventListener("click", () => {
-  navbar.classList.toggle("active")
+  navbar.classList.toggle("active");
   menuToggleBtn.classList.toggle("active");
 });
 
@@ -24,7 +24,7 @@ for (let i = 0; i < navbarLinks.length; i++) {
 // header sticky
 
 const header = document.querySelector("[data-header]");
-console.log(header)
+console.log(header);
 
 window.addEventListener("scroll", () => {
   if (window.scrollY >= 100) {
@@ -33,3 +33,18 @@ window.addEventListener("scroll", () => {
     header.classList.remove("active");
   }
 });
+
+// searchbox toggel
+
+const searchBtn = document.querySelector("[data-search-btn]");
+const searchContainer = document.querySelector("[data-search-container]");
+const searchSubmitBtn = document.querySelector("[data-search-submit-btn]");
+const searchCloseBtn = document.querySelector("[data-search-close-btn]");
+
+const searchBoxElement = [searchBtn, searchSubmitBtn, searchCloseBtn];
+for (let i = 0; i < searchBoxElement.length; i++) {
+  searchBoxElement[i].addEventListener("click", () => {
+    searchContainer.classList.toggle("active");
+    document.body.classList.toggle("active");
+  });
+}
